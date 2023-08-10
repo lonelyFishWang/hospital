18fcad81cb0a9386045eba99a9577322208480f7
王福雨 <15504910601@163.com>
2023-08-10 21:54:06 +0800

    8-10-21:53

---
+++ service/service-hosp/src/main/java/com/atguigu/hosp/mapper/HospitalSetMapper.java
@@ -0,0 +1,18 @@
+package com.atguigu.hosp.mapper;
+
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author wangfuyu
+* @description 针对表【hospital_set(医院设置表)】的数据库操作Mapper
+* @createDate 2023-08-10 11:48:21
+* @Entity .HospitalSet
+*/
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
+
+
+
+
