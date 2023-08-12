8c68ef38a58b01c7228e2efb6507e6d5dd3d4986
王福雨 <15504910601@163.com>
2023-08-12 10:36:27 +0800

    8月12 上午十点准备写分页

--- common/comon-util/src/main/java/com/atguigu/common/result/Result.java
+++ common/comon-util/src/main/java/com/atguigu/common/result/Result.java
@@ -1,4 +1,23 @@
 package com.atguigu.common.result;
 
 public class Result {
+
+
+    private String code;
+    private String msg;
+    private Object data;
+
+
+    public Result() {
+    }
+
+    public Result(String code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+    public static Result success(String code,String  msg,Object data){
+        return new Result(code, msg, data) ;
+    }
+
 }
